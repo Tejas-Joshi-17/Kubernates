8f043ca7fea93a27508e664bbf14123ff7cb29db
Tejas-Joshi-17 <joshitejas188@gmail.com>
2025-03-09 14:23:25 +0530

    Project-1 Learning Kubernetes Added

---
+++ Project-1/testapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is <code>Project-1</code> of Learning Kubernates <b>Version-2</b>.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
